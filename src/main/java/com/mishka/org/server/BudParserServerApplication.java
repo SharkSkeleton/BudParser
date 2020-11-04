8689a912c76e8ea0ed2feeae8ba1a9d4a539c524
mishka <mishamyagkiy3@gmail.com>
2020-11-04 19:22:16 +0200

    Template created

---
+++ src/main/java/com/mishka/org/server/BudParserServerApplication.java
@@ -0,0 +1,13 @@
+package com.mishka.org.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BudParserServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BudParserServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mishka/org/server/BudParserServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mishka.org.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BudParserServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
